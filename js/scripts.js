dd6db578fcfe3df62a8343cb2c569ad5dbd2c2e6
Swathi-prog <59515233+Swathi-prog@users.noreply.github.com>
2020-01-23 10:02:02 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Hi! Welcome to the portfolio site of 'Masetty's design'-A Design for tomorrow");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
